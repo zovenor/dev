82036956ff7d9b694ffb7f714f0d1d797aa6ffc2
Alexandr <romanenyasasha@gmail.com>
2024-06-14 12:06:12 +0300

    Upload lazyvim conf

---
+++ nvim/lazyvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+  -- add gruvbox
+  { "folke/tokyonight.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "night" },
+  },
+}
